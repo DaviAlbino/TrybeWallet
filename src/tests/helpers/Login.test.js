866d14412b5f2612716a3137adad6fef58ebb6b1
Davi Albino <negroalbino_@hotmail.com>
2022-08-05 00:15:33 -0300

    Ainda tentando resolver package-json

---
+++ src/tests/helpers/Login.test.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import Login from "../../pages/Login";
+// import { renderWithRouterAndRedux } from "./renderWith";
+
+// describe('Testes para validar a página Login', () => {
+//     it('', () => {
+//         renderWithRouterAndRedux(<Login />)
+        
+//         const emailInput = screen.getByTestId("email-input");
+//         const passwordInput = screen.getByTestId("password-input");
+//         const signBtn = screen.getByRole('button');
+
+//         expect(emailInput).toBeInTheDocument();
+//     })
+// })
